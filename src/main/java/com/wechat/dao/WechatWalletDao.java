dc18b22c45b6a3f1801c109f6a56131fde607d53
liyixing1 <liyixingx@qq.com>
2016-03-22 18:30:27 +0800

    是否认证

--- src/main/java/com/wechat/dao/WechatWalletDao.java
+++ src/main/java/com/wechat/dao/WechatWalletDao.java
@@ -11,4 +11,11 @@ public interface WechatWalletDao {
 	WechatWallet getByexternalNo(String externalNo);
 	
 	void update(Long id ,String username, String wechatPayNo);
+	
+	/**
+	 * 
+			* 描述:设置认证
+			* @author liyixing 2016年3月22日 下午1:39:27
+	 */
+	void setIdentificationFlag(Long id ,Boolean identificationFlag);
 }
--- src/main/java/com/wechat/manager/WechatWalletMng.java
+++ src/main/java/com/wechat/manager/WechatWalletMng.java
@@ -10,5 +10,12 @@ public interface WechatWalletMng {
 	
 	WechatWallet update(Long id ,String username, String wechatPayNo);
 	
+	/**
+	 * 
+			* 描述:设置认证
+			* @author liyixing 2016年3月22日 下午1:39:27
+	 */
+	void setIdentificationFlag(Long id ,Boolean identificationFlag);
+	
 	WechatWallet getByexternalNo(String externalNo);
 }
