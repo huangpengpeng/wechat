bdc428c932656d0ac3317a9b4c144a0b72859902
fangzhilong <1621006784@qq.com>
2017-02-09 13:58:02 +0800

    init

--- src/main/java/com/wechat/dao/PartnerDao.java
+++ src/main/java/com/wechat/dao/PartnerDao.java
@@ -11,7 +11,7 @@ public interface PartnerDao {
 	public void delete(Long id);
 	
 	public void update(Long id, String name, String appId, String secretKey, String mchId, String signKey,
-			String deviceInfo, String realm, String token, Date registerTime, String callUrl);
+			String deviceInfo, String realm, String token, Date registerTime, String callUrl); 
 	
 	Partner getByToken(String token);
 
